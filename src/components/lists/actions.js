56ff23ab9d593c14dab9946e6938bc5f8b80890e
trickstival <trick_stival@hotmail.com>
2019-05-30 21:43:02 -0300

    Added manual prefetch

--- src/components/lists/actions.js
+++ src/components/lists/actions.js
@@ -1,4 +1,11 @@
 import SceneEntity from '@/components/scene-use/SceneEntity'
+import thePirateImage from '@/assets/the-pirate-example.jpg'
+import jsCaptureTheFrameworksImage from '@/assets/js-capture.gif'
+
+prefetchImages([
+    thePirateImage,
+    jsCaptureTheFrameworksImage
+])
 
 const image = (id, src, url) => {
     return SceneEntity(id, {
@@ -27,6 +34,8 @@ const image = (id, src, url) => {
     })
 }
 
+// Open-source project image sources
+
 // if a function is yielded, it should wait for the user to click on "Go Next"
 export default async function* (scene) {
     // Intro
@@ -39,15 +48,25 @@ export default async function* (scene) {
     patrick.goTo({ left: 0 })
 
     // the-pirate
-    const entityImage = image('the-pirate', require('@/assets/the-pirate-example.jpg'), 'https://github.com/trickstival/Javascript-Capture-The-Frameworks')
+    const entityImage = image('the-pirate', thePirateImage, 'https://github.com/trickstival/Javascript-Capture-The-Frameworks')
     await scene.addEntity(entityImage)
     entityImage.moveTo({ top: '25%', right: '15px' })
     yield patrick.speak('This is how you use my lib called "the-pirate"')
 
     // JS Capture the frameworks
-    entityImage.componentInstance.src = require('@/assets/js-capture.gif')
+    entityImage.componentInstance.src = jsCaptureTheFrameworksImage
     entityImage.componentInstance.url = 'https://github.com/trickstival/Javascript-Capture-The-Frameworks'
     yield patrick.speak('JS Capture the Frameworks is a game I made based on pacman with divs!')
 
     yield patrick.speak('This other "project" is just shrek spinning.. HEHE')
+}
+
+function prefetchImages (images) {
+    for (const image of images) {
+        const link = document.createElement('link')
+        link.setAttribute('rel', 'prefetch')
+        link.setAttribute('as', 'image')
+        link.setAttribute('href', image)
+        document.head.appendChild(link)
+    }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
-const PreloadWebpackPlugin = require('preload-webpack-plugin')
+// const PreloadWebpackPlugin = require('preload-webpack-plugin')
 
-module.exports = {
-    configureWebpack: {
-        plugins: [
-            new PreloadWebpackPlugin({
-                rel: 'preload',
-                as: 'image'
-            })
-        ]
-    }
-}
+// module.exports = {
+//     configureWebpack: {
+//         plugins: [
+//             new PreloadWebpackPlugin({
+//                 rel: 'prefetch',
+//                 as: 'image',
+//                 include: ['hey']
+//             })
+//         ]
+//     }
+// }
+
+// Added manual prefetch since it doesn't work for images right now
